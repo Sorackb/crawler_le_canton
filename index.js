65c40964b1a42282e04f30614fef95fb1648b849
Lucas Bernardo <ti.desenvolvimento@madalozzocorretora.com.br>
2018-12-20 17:38:05 -0200

    Adicionado CORS

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const principal = require('./rota/principal');
 
 const app = express();
 
 app.set('trust proxy', true);
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 // URLs que não sejam de serviço são entregues como arquivos públicos dentro da pasta "publico"
 app.use(express.static('./publico'));
