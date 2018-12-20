3c2da60bb677e6fcac77c92c738efaf79386c360
Sorackb <sorackb@gmail.com>
2018-12-19 23:26:49 -0200

    Adicionados testes unitários

---
+++ test/main.js
@@ -0,0 +1,45 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const servidor = require('../');
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('/POST buscar', () => {
+  it('Deve buscar as informações de quartos disponíveis', function(done) {
+    this.timeout(30000);
+
+    chai.request(servidor).post('/buscar').send({ checkin: '01/01/2019', checkout: '05/01/2019' }).end(function(err, res) {
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+
+      done();
+    });
+  });
+
+  it('Não deve retornar informações', function(done) {
+    this.timeout(30000);
+
+    chai.request(servidor).post('/buscar').send({ checkin: '18/12/2018', checkout: '19/12/2018' }).end(function(err, res) {
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+
+      done();
+    });
+  });
+
+  it('O formato de data deve ser inválido para o serviço', function(done) {
+    this.timeout(30000);
+
+    chai.request(servidor).post('/buscar').send({ checkin: 'AA/12/2018', checkout: 'AA/12/2018' }).end(function(err, res) {
+      res.should.have.status(500);
+
+      done();
+    });
+  });
+
+  after(async () => {
+    await servidor.parar();
+  });
+});
